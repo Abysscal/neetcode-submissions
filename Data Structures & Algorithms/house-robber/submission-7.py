6bf209dddc7f19973e81e979cb2b69eda604e3bd
Abysscal <35967401+Abysscal@users.noreply.github.com>
2026-08-28 18:14:24 +0800

    Add: house-robber - submission-7

---
+++ Data Structures & Algorithms/house-robber/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) <= 2:
+            return max(nums)
+        first = nums[0]
+        second = max(nums[0], nums[1])
+        dp = [-1] * (len(nums))
+        dp[0] = first
+        dp[1] = second
+
+        def dps(unit):
+            if dp[unit] != -1:
+                return dp[unit]
+            dp[unit] = max(dps(unit-2) + nums[unit], dps(unit-1))
+
+        for i in range(2, len(nums)):
+            dps(i)
+
+        return max(dp)
